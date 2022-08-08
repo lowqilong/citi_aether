8e7bfbbe7f53175c62b29696510e6c5636709a3e
Keith Tan <KT0C104824@SISIHBLP00567.local>
2022-08-08 21:35:02 +0800

    added dynamodb

--- backend/connect/app.js
+++ backend/connect/app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const serverless = require('serverless-http');
 const app = express()
 const port = 4000
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 app.get('/connect', (req, res) => {
-    res.send('Connect')
+    // var obj = {'username' : 'keith', 'password' : 'keithpw'};
+    var obj = {
+        TableName: "CitiUsers",
+        Item: {'username' : 'keith', 'password' : 'keithpw'}
+    };
+    dynamoDB.put(obj);
+    res.send('Connect' + JSON.stringify(obj));
 })
 
 // app.listen(port, () => {
