e5abd130663cad73bd1e8b2ac72a93f765d887d0
lowqilong <lowqilong@hotmail.com>
2022-08-08 20:25:34 +0800

    fix routes

--- backend/connect/app.js
+++ backend/connect/app.js
@@ -3,12 +3,12 @@ const serverless = require('serverless-http');
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
+app.get('/connect', (req, res) => {
     res.send('Connect')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
 
 module.exports.handler = serverless(app);
