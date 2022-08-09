86dc8ea7ae47f1afd07e8dd20e00fa3fc383e4f3
Uygnis <vesperlsy@gmail.com>
2022-08-09 19:20:17 +0800

    ..

--- frontend/my-app/src/components/Navbar/Navbar.js
+++ frontend/my-app/src/components/Navbar/Navbar.js
@@ -2,46 +2,73 @@ import React, { useState } from "react";
 import { AccountItems } from "./AccountItems";
 import { MenuItems } from "./MenuItems";
 import { Button } from "../Button";
-import { Link } from "react-router-dom"
-import "./Navbar.css"
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
-    const [toggleMenu, setToggle] = useState(false)
-    const [clicked, setClicked] = useState("")
-    const [toggleDropDown, setDropDown] = useState(false)
+  const [toggleMenu, setToggle] = useState(false);
+  const [clicked, setClicked] = useState("");
+  const [toggleDropDown, setDropDown] = useState(false);
 
-    return (
-        <nav className="NavbarItems">
-            <h2 className="navbar-logo">
-                AETHER FINANCE
-                <img src ={require("../../images/Penguin.png")} alt="Aether Finance Logo" className="logo-image"/>
-            </h2>
-            <div className="menu-icon" onClick={() => setToggle(!toggleMenu)}>
-                <i className={toggleMenu ? 'fas fa-times' : 'fas fa-bars'}></i>
-            </div>
-            <ul className={toggleMenu ? "nav-menu active" : "nav-menu"}>
-                {MenuItems.map((item, index) => {
-                    return (
-                        <li key={index} onClick={() => setClicked(item.title)}>
-                            <Link className={item.cName === "nav-links" ? clicked===item.title ? "nav-links active" : "nav-links" : "nav-links-mobile"} to={item.url}>
-                                {item.title}
-                            </Link>
-                        </li>
-                    )
-                })}
-            </ul>
-            <img src = {require("../../images/profilepic.png")} alt="Profile Picture" className="profile-picture" onClick={() => setDropDown(!toggleDropDown)}/>
-            <ul className={toggleDropDown ? "account-menu active" : "account-menu"}>
-                {AccountItems.map((item, index) => {
-                    return (
-                            <li key={index} onClick={() => setClicked(item.title)}>
-                                <Link className={item.cName === "nav-links" ? clicked===item.title ? "nav-links active" : "nav-links" : "nav-links-mobile"} to={item.url}>
-                                    {item.title}
-                                </Link>
-                            </li>
-                    )
-                })}
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="NavbarItems">
+      <h2 className="navbar-logo">
+        AETHER FINANCE
+        <img
+          src={require("../../images/Penguin.png")}
+          alt="Aether Finance Logo"
+          className="logo-image"
+        />
+      </h2>
+      <div className="menu-icon" onClick={() => setToggle(!toggleMenu)}>
+        <i className={toggleMenu ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={toggleMenu ? "nav-menu active" : "nav-menu"}>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index} onClick={() => setClicked(item.title)}>
+              <Link
+                className={
+                  item.cName === "nav-links"
+                    ? clicked === item.title
+                      ? "nav-links active"
+                      : "nav-links"
+                    : "nav-links-mobile"
+                }
+                to={item.url}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <img
+        src={require("../../images/profilepic.png")}
+        alt="Profile Picture"
+        className="profile-picture"
+        onClick={() => setDropDown(!toggleDropDown)}
+      />
+      <ul className={toggleDropDown ? "account-menu active" : "account-menu"}>
+        {AccountItems.map((item, index) => {
+          return (
+            <li key={index} onClick={() => setClicked(item.title)}>
+              <Link
+                className={
+                  item.cName === "nav-links"
+                    ? clicked === item.title
+                      ? "nav-links active"
+                      : "nav-links"
+                    : "nav-links-mobile"
+                }
+                to={item.url}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
