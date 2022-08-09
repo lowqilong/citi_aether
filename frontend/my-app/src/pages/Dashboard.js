be9dae12d838a691d110b87eca5719ccc79e04c8
lowqilong <lowqilong@hotmail.com>
2022-08-09 20:33:13 +0800

    initialise chartjs

--- frontend/my-app/src/pages/Dashboard.js
+++ frontend/my-app/src/pages/Dashboard.js
@@ -1,9 +1,58 @@
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
 
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top'
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => Math.random()*1000),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        tension: 0.2
+      }
+    ],
+  };
 
 export function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
+            <Line options={options} data={data} />
         </div>
     );
 }
