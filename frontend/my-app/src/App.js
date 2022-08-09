58ff727eef2948c1f21369c2ebed6df0c6aa95a6
lowqilong <lowqilong@hotmail.com>
2022-08-09 11:22:00 +0800

    amplify

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import {Navbar, Home, About, News, Footer} from "./components/index"
+import { Navbar, Home, About, News, Footer } from "./components/index"
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
-function App() {
+function App({ signOut, user }) {
   return (
     <div className="App">
       <Navbar />
-      <Home/>
-      <About/>
-      <News/>
-      <Footer/>
+      <Home />
+      <About />
+      <News />
+      <Footer />
     </div>
   );
 }
