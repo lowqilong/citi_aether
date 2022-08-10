a4dcac88b646f24ba209f442d99ada542dfe6a2f
Keith Tan <KT0C104824@SISIHBLP00567.local>
2022-08-10 13:47:05 +0800

    setting.js edits

--- frontend/my-app/src/pages/Setting.js
+++ frontend/my-app/src/pages/Setting.js
@@ -1,7 +1,21 @@
+import { Button } from "../components/Button";
 export function Setting() {
     return (
         <div>
-            <h1>Setting</h1>
+            <h1>Settings</h1>
+            <section>
+                <h1>Change Rounding Value</h1>
+                <h3>Your Current Rounding Value is:</h3>
+
+                <Button>Update</Button>
+            </section>
+            <section>
+                <h1>Change Main Investment</h1>
+                <h3>When you make payments for online purchases, we will round to the nearest:</h3>
+                <h3>The difference will be contributed to your Aether Account</h3>
+                <Button>Update</Button>
+            </section>
+            
         </div>
     );
 }
