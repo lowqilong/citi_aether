343596b70a4e1960f6e8ec74b50401f912af5483
BaoShinYeo <baoshin.yeo.2019@accountancy.smu.edu.sg>
2022-08-11 01:00:39 +0800

    Added in placeholder text for Education

--- frontend/my-app/src/pages/Education.js
+++ frontend/my-app/src/pages/Education.js
@@ -3,7 +3,35 @@ export function Education() {
     return (
         <section>
             <h1>Why should you get started?</h1>
-            <p>If you want to make the most of your spare change and get the occasional retailer kickback, there’s really no better place to do that than here at Aether Finance. The automatic roundups makes saving and investing easy. As a retail investor, you will be surprised by how quickly those pennies accumulate.</p>
+            <br />
+            <p>
+                If you want to make the most of your spare change and get the occasional retailer kickback, 
+                there’s really no better place to do that than here at Aether Finance. 
+                The automatic roundups makes saving and investing easy. 
+                As a retail investor, you will be surprised by how quickly those pennies accumulate.
+            </p>
+            <br />
+            <br />
+            <br />
+            <h1>What is Investing?</h1>
+            <br />
+            <p>
+                Investing, broadly, is putting money to work for a period of time in some sort of project or undertaking in order to generate positive returns 
+                (i.e., profits that exceed the amount of the initial investment). It is the act of allocating resources, usually capital (i.e., money), 
+                with the expectation of generating an income, profit, or gains.
+            </p>
+            <br />
+            <p>
+                One can invest in many types of endeavors (either directly or indirectly) such as using money to start a business, 
+                or in assets such as purchasing real estate in hopes of generating rental income and/or reselling it later at a higher price.
+            </p>
+            <br />
+            <p>
+                Investing differs from saving in that the money used is put to work, meaning that there is some implicit risk that the related project(s) may fail, 
+                resulting in a loss of money. Investing also differs from speculation in that with the latter, the money is not put to work per-se, 
+                but is betting on the short-term price fluctuations.
+            </p>
+            <br />
         </section>
     );
 }
